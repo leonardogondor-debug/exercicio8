25d43cb8f1b29e4cc008dc5865b4a61fbc642f52
leovit <leonardogondor@gmail.com>
2025-09-19 07:04:36 -0300

    js atualizacategoria atualizavalorcategoria atualizainteface teste

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ const obterDados = (id) => document.getElementById(id).value;
 const pegarCategoria = () => obterDados("categoria");
 const pegarValor = () => parseFloat(obterDados("valor"));
 const valornegativo = (valor) => valor < 0;
-const valortotal = (total , valor) => total + valor;
+const valorSoma = (total , valor) => total + valor;
+
+const atualizaCategoria = (matriz, nomeCategoria) => matriz.find((item) => item[0] === nomeCategoria);
+
+const atualizarvalorCategoria = (categoria, valor) => categoria[1] = somaValor(categoria[1], valor);
 
 const atualizarInterface = () => {
     matriztabela.forEach(([nome, valor]) => {
       const elemento = obterDados(nome);
-      elemento.textContent = '${nome}: R$ ${valor}';
+      elemento.textContent = `${nome}: R$ ${valor}`;
     }
 )}
 
@@ -24,6 +28,6 @@ const atualizarInterface = () => {
 function teste() {
     const valor = pegarValor();
     const categoria = pegarCategoria();
-    document.getElementById("categoria").innerText="${categoria}: $ r${valor}"; 
+    document.getElementById("alimentacao").innerText=`${categoria}: R$${valor}`; 
     return;
 }
