3ece9d2dca3afe60689cb365fb7707785f3929b6
leovit <leonardogondor@gmail.com>
2025-09-19 23:26:30 -0300

    acrecimos JS

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const matriztabela = [
 const obterDados = (id) => document.getElementById(id).value;
 const pegarCategoria = () => obterDados("categoria");
 const pegarValor = () => parseFloat(obterDados("valor"));
+const limparcampo = () => obterDados ("valor").value = '';
 const valornegativo = (valor) => valor < 0;
 const valorSoma = (total , valor) => total + valor;
 
-const atualizaCategoria = (matriz, nomeCategoria) => matriz.find((item) => item[0] === nomeCategoria);
+const atualizaCategoria = (matriz, nomeCategoria) => matriz.find((item) => item[0] === nomeCategoria).value;
 
-const atualizarvalorCategoria = (categoria, valor) => categoria[1] = somaValor(categoria[1], valor);
+const atualizarvalorCategoria = (categoria, valor) => categoria[1] = somaValor(categoria[1], valor).value;
 
 const atualizarInterface = () => {
     matriztabela.forEach(([nome, valor]) => {
@@ -26,8 +27,16 @@ const atualizarInterface = () => {
 
 
 function teste() {
-    const valor = pegarValor();
-    const categoria = pegarCategoria();
-    document.getElementById("alimentacao").innerText=`${categoria}: R$${valor}`; 
-    return;
+    const valorInformado = pegarValor();
+    const categoriaInformado = pegarCategoria();
+    if (valornegativo(valorInformado)){
+       alert('valor informado nao aceito!');
+       return;
+    }
+    const categoria = pegarCategoria(matriztabela, categoriaInformado)
+    const total = pegarCategoria(matriztabela, "total");
+    atualizarvalorCategoria(categoria, valorInformado);
+    atualizarvalorCategoria(total, valorInformado);
+    atualizarInterface();
+    limparcampo();
 }
